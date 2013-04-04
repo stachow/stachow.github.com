d7fc8edb8c2e938cebe9d152cb1525b66f47a511
stachow <stef@stachow.co.uk>
2013-04-04 04:59:40 +0100

    Grav at bottom

--- js/HomeCtrl.js
+++ js/HomeCtrl.js
@@ -1,13 +1,11 @@
 function HomeCtrl($scope, $http) {
     $http.jsonp('https://api.github.com/users/stachow/repos?callback=JSON_CALLBACK')
     .success(function (msg) {
-        var myRepos = _.chain(msg.data)
-                    .filter(function (repo) { return !repo.fork && repo.name !== 'stachow.github.com' })
-                    .sortBy('created_at')
-                    .reverse()
-                    .value();
-
-        console.log(myRepos);
+        var myRepos =  _.chain(msg.data)
+                        .filter(function (item) { return !item.fork && item.name !== 'stachow.github.com' })
+                        .sortBy(function (item) { return (item.name.indexOf('grav') !== -1 ? '0000-00-00' : item.created_at) })
+                        .reverse()
+                        .value();
         $scope.repos = myRepos;
     });
 
